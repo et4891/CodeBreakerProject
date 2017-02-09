084da403a75c3a6ee850ce1eae904f94114d8b62
et4891 <et1103@hotmail.com>
2017-02-08 23:36:32 -0800

    step 6 - on going

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,7 +8,21 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields() {
+    // let answer = Math.floor((Math.random() * 10000)).toString();
+    // let maxAnswerLength = 4;
+    // let lengthOfAnswer = answer.length;
+    //
+    // if((lengthOfAnswer < maxAnswerLength)){
+    //     let prefixZeros = maxAnswerLength - lengthOfAnswer;
+    // }
+    // return answer;
     let answer = Math.floor((Math.random() * 10000)).toString();
-
+    while(answer.length < 4){
+        answer = '0' + answer
+    }
     return answer;
+}
+
+for (let i = 0; i < 11; i++){
+    console.log(setHiddenFields());
 }
