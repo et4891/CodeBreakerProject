a4a6d5e18e1b73f44ee97691b41b7f7e1209e754
ET-Asus <et1103@hotmail.com>
2017-02-13 15:18:17 -0800

    added comments

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,6 +7,12 @@ function guess() {
 }
 
 //implement new functions here
+
+/*
+* setHiddenFields()
+* Generate random numbers from 0 - 9999
+* If the number generated is less than 1 digit, will automatically add 0s in front to make it 4 digits
+* */
 function setHiddenFields() {
     answer.value = Math.floor((Math.random() * 10000)).toString();
     while(answer.value.length < 4){
